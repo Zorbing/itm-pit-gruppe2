0b47717bee4f56e343cb1628bedac1864b39d70a
Zorbing <zorbing@gmx.net>
2016-05-18 10:16:40 +0200

    removed jars with source of pi4j changed pin of led

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,34 +1,5 @@
 package main;
 
-/*
-* #%L
-* **********************************************************************
-* ORGANIZATION  :  Pi4J
-* PROJECT       :  Pi4J :: Java Examples
-* FILENAME      :  ControlGpioExample.java  
-* 
-* This file is part of the Pi4J project. More information about 
-* this project can be found here:  http://www.pi4j.com/
-* **********************************************************************
-* %%
-* Copyright (C) 2012 - 2015 Pi4J
-* %%
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Lesser General Public License as
-* published by the Free Software Foundation, either version 3 of the
-* License, or (at your option) any later version.
-* 
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Lesser Public License for more details.
-* 
-* You should have received a copy of the GNU General Lesser Public
-* License along with this program.  If not, see
-* <http://www.gnu.org/licenses/lgpl-3.0.html>.
-* #L%
-*/
-
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalOutput;
@@ -51,7 +22,7 @@ public class Main {
        final GpioController gpio = GpioFactory.getInstance();
        
        // provision gpio pin #01 as an output pin and turn on
-       final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.HIGH);
+       final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_08, "MyLED", PinState.HIGH);
        
        // set shutdown state for this pin
        pin.setShutdownOptions(true, PinState.LOW);
