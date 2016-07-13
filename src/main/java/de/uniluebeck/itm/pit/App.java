a2fec17583c73aa03b21f9e94eb883fd2be75ad6
Martin Boekhoff <zorbing@gmx.net>
2016-07-13 02:39:55 +0200

    tidied up brightness detector
    moved ssp-logic to class App to make it easier to reuse the server
    instance
    implemented Observable in class BrightnessDetector

--- src/main/java/de/uniluebeck/itm/pit/App.java
+++ src/main/java/de/uniluebeck/itm/pit/App.java
@@ -1,17 +1,51 @@
 package de.uniluebeck.itm.pit;
 
-public class App
+import java.net.URISyntaxException;
+import java.util.Observable;
+import java.util.Observer;
+
+import de.uniluebeck.itm.pit.ncoap.LedObservableWebservice;
+import de.uniluebeck.itm.pit.ncoap.SimpleCoapServer;
+
+public class App implements Observer
 {
+	private static final int lifeTime = 60 * 60 * 24 * 365;
+	
+	private LedObservableWebservice ledService;
+	private SimpleCoapServer server;
+	
+	public App() throws URISyntaxException
+	{
+		// create coap server
+		server = new SimpleCoapServer();
+		// create web service for led state
+		ledService = new LedObservableWebservice("/led", server.getExecutor());
+		
+		// register led-webservice
+		server.registerService(ledService);
+		// register all webservices at the SSP
+		server.registerAtSSP();
+	}
+	
 	public static void main(String[] args) throws Exception
 	{
 		System.out.println("Program started v3.");
 		
 		BrightnessDetector detector = new BrightnessDetector();
-		detector.start();
+		Observer observer = new App();
+		detector.addObserver(observer);
 		
 		while (true)
 		{
 			Thread.sleep(500);
 		}
+	}
+	
+	@Override
+	public void update(Observable o, Object arg)
+	{
+		boolean isDark = ((BrightnessDetector) o).isDark();
+		System.out.println(String.format("led state on: %b", isDark));
+		ledService.setResourceStatus(isDark, lifeTime);
 	}
 }
--- src/main/java/de/uniluebeck/itm/pit/BrightnessDetector.java
+++ src/main/java/de/uniluebeck/itm/pit/BrightnessDetector.java
@@ -1,6 +1,6 @@
 package de.uniluebeck.itm.pit;
 
-import java.net.URISyntaxException;
+import java.util.Observable;
 
 import com.pi4j.io.gpio.RaspiPin;
 import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
@@ -8,51 +8,43 @@ import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 
 import de.uniluebeck.itm.pit.hardware.Ldr;
 import de.uniluebeck.itm.pit.hardware.Led;
-import de.uniluebeck.itm.pit.ncoap.LedObservableWebservice;
 import de.uniluebeck.itm.pit.ncoap.LoggingConfiguration;
-import de.uniluebeck.itm.pit.ncoap.SimpleCoapServer;
 
-public class BrightnessDetector implements GpioPinListenerDigital
+public class BrightnessDetector extends Observable implements GpioPinListenerDigital
 {
-	private static final int lifeTime = 60 * 60 * 24 * 365;
-	private LedObservableWebservice ledService;
+	private boolean lastState;
 	private Ldr ldr;
 	private Led led;
-	private SimpleCoapServer server;
 	
 	public BrightnessDetector() throws Exception
 	{
 		// initialize the simple coap server
 		LoggingConfiguration.configureDefaultLogging();
 		
-		// create coap server
-		server = new SimpleCoapServer();
-		// create web service for led state
-		ledService = new LedObservableWebservice("/led", server.getExecutor());
-		
 		ldr = new Ldr(RaspiPin.GPIO_07);
 		led = new Led(RaspiPin.GPIO_08);
+		
+		// add change listener
+		ldr.addChangeListener(this);
+		// call the change handler one first time
+		this.handleGpioPinDigitalStateChangeEvent(null);
+	}
+	
+	public boolean isDark()
+	{
+		return lastState;
 	}
 	
 	@Override
 	public void handleGpioPinDigitalStateChangeEvent(GpioPinDigitalStateChangeEvent event)
 	{
-		boolean state = ldr.isHigh();
-		led.setOn(!state);
-		System.out.println(String.format("led state on: %b", !state));
-		ledService.setResourceStatus(state, lifeTime);
-	}
-	
-	public void start() throws URISyntaxException
-	{
-		// register led-webservice
-		server.registerService(ledService);
-		// register all webservices at the SSP
-		server.registerAtSSP();
-		
-		// add change listener 
-		ldr.addChangeListener(this);
-		// call the change handler one first time
-		this.handleGpioPinDigitalStateChangeEvent(null);
+		boolean state = !ldr.isHigh();
+		led.setOn(state);
+		if (state != lastState)
+		{
+			lastState = state;
+			setChanged();
+			notifyObservers();
+		}
 	}
 }
